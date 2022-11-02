48a3d56248a41e357f1e5323297c649e1b2b6465
呂 博多 <blyu@ryonoMacBook-Air.local>
2022-11-02 21:27:59 +0900

    ...

--- 01/ex00/main.cpp
+++ 01/ex00/main.cpp
@@ -5,7 +5,7 @@ int main(void)
     Zombie st("stack_zombie");
     Zombie* hp = newZombie("heap_zombie");
     
-    randomChump("stack_zombie");
+    randomChump("randomChump_zombie");
     st.announce();
     hp->announce();
     delete hp;
--- 01/ex00/randomChump.cpp
+++ 01/ex00/randomChump.cpp
@@ -1,10 +1,9 @@
 #include "Zombie.hpp"
 #include <iostream>
-#include <string>
 
 void randomChump(std::string zombie_name)
 {
-    Zombie z("randomChump_zombie");
+    Zombie z(zombie_name);
     std::cout<<"My name is "<< z.get_name() << std::endl;
     return ;
 }
